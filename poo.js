3041deef5f867ede5186b2608e85445769af973b
unknown <jessi.josephine@gmail.com>
2022-07-12 10:33:45 -0400

    class oiseau ok

--- poo.js
+++ poo.js
@@ -25,3 +25,42 @@ class animal {
         console.log("L'animal" + this.nom + "communique..");
     }
 }
+
+class oiseau extends Animal {
+    constructor(nom,couleur, sante, longueurDesAiles){
+    super(nom, couleur, sante);
+    
+    this.longueurDesAiles = longueurDesAiles;
+}
+
+Voler(){
+    super.seDeplacer();
+    console.log("L'animal" + this.nom + "vole.");
+}
+
+Chanter(){
+    super.communiquer();
+    console.log("L'oiseau" + this.nom + "chante.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

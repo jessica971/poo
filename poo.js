95ceae86e76be15d6841f3498dc00008d6cc7374
unknown <jessi.josephine@gmail.com>
2022-07-12 09:43:56 -0400

    Premier commit

--- poo.js
+++ poo.js
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-
-<html>
-
-
-
-<head>
-
-    <meta charset="utf-8" />
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>TP Citations</title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="stylesheet" type="text/css" media="screen" href="style/styles (1).css" />
-
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Satisfy">
-
-    <!-- <script src="javascript/cookie.js"></script> -->
-
-</head>
+class animal {
+    
+}
